404ba0a59766e7680448079d7dde83a3f8fac237
Jônatas <jonatasthome04@gmail.com>
2025-05-26 19:02:43 -0300

    Started JS - Query selectors

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Seleção de Elementos
+const todoForm = document.querySelector("#todo-form")
+const todoInput = document.querySelector("#todo-input")
+const todoList = document.querySelector("#todo-lisr")
+const editForm = document.querySelector("#edit-form")
+const editInput = document.querySelector("#edit-input")
+const cancelEditBtn = document.querySelector("#cancel-edit-btn")
+
