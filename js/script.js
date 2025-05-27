c3ce6c7656828b370f2ecfd65d1d0eaccbac54af
Jônatas <jonatasthome04@gmail.com>
2025-05-27 11:57:31 -0300

    Updated JS - Edit event

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+let oldInputValue;
+
 
 //Funções
 const saveToDo = (text) => {
@@ -55,6 +57,12 @@ todoForm.addEventListener("submit", (e) =>{
 document.addEventListener("click", (e) =>{
     const targetEl = e.target
     const parentEl = targetEl.closest("div")
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
+
     if (targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("done")
     }
@@ -63,6 +71,8 @@ document.addEventListener("click", (e) =>{
     }
     if(targetEl.classList.contains("edit-todo")){
         toggleForms();
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
     }
 })
 
