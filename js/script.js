2483b8494c000d850d4893ac389974f15fe8e4ef
Jônatas <jonatasthome04@gmail.com>
2025-05-26 23:24:13 -0300

    Updated JS - Started to implement functions

--- js/script.js
+++ js/script.js
@@ -6,12 +6,29 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+
+//Funções
+const saveToDo = (text) =>{
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+    
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fas fa-check"></i>'
+    
+}
+
+//Eventos
 todoForm.addEventListener("submit", (e) =>{
     e.preventDefault();
     console.log("Form enviado!")
 
     const inputValue = todoInput.value
     if(inputValue){
-        console.log(inputValue)
+        saveToDo(inputValue)
     }
 })
